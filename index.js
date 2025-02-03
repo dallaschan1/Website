81c894af05823cdcb475d1a7bb5344f112750717
Dallas <S10258596@connect.np.edu.sg>
2025-02-04 06:32:46 +0800

    sd

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
